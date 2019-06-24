9e0a0d821c48fc4b0e0745bdf31eac38cce5bb4a
Andrea <andrea.belluccia@gmail.com>
2019-06-24 17:22:20 +0200

    Created upload route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const router = Router();
 
+const Image = require("../models/Image");
+
 router.get("/", (req, res) => {
   res.send("Index Page");
 });
@@ -9,9 +11,19 @@ router.get("/upload", (req, res) => {
   res.render("upload");
 });
 
-router.post("/upload", (req, res) => {
-  console.log(req.file);
-  res.send("Uploaded");
+router.post("/upload", async (req, res) => {
+  const image = new Image();
+  image.title = req.body.title;
+  image.description = req.body.description;
+  image.filename = req.file.filename;
+  image.path = "/img/uploads/" + req.file.filename;
+  image.originalname = req.file.originalname;
+  image.mimetype = req.file.mimetype;
+  image.size = req.file.size;
+
+  await image.save();
+
+  res.redirect("/");
 });
 
 router.get("/image/:id", (req, res) => {
